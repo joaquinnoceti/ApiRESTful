79e2aaf357364dace2482993fbe58485aca2cbc6
Joaquin <joaquinnoceti@hotmail.com>
2024-07-10 15:34:06 -0300

    validaciones por modelo, implementacion de interfaz validatable

--- WebApi/Entidades/Autor.cs
+++ WebApi/Entidades/Autor.cs
@@ -4,13 +4,24 @@ using WebApi.Validaciones;
 
 namespace WebApi.Entidades
 {
-    public class Autor
+    public class Autor : IValidatableObject
     {
         public int ID { get; set; }
-        [Required (ErrorMessage = "El campo nombre es obligatorio.")] //(ErrorMessage = "El campo {0} es obligatorio.")
-        [StringLength(maximumLength:10,ErrorMessage ="El campo {0} debe ser menor a {1} caracteres")]
-        [ValidacionesNombre] //validacion personalizada x atributo
+        [Required(ErrorMessage = "El campo nombre es obligatorio.")] //(ErrorMessage = "El campo {0} es obligatorio.")
+        [StringLength(maximumLength: 10, ErrorMessage = "El campo {0} debe ser menor a {1} caracteres")]
+   //     [ValidacionesNombre] //validacion personalizada x atributo
         public string Nombre { get; set; }
         public List<Libro> Libros { get; set; }
+
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            if (!string.IsNullOrEmpty(Nombre))
+            {
+                var Letra = Nombre[0].ToString();
+                if (Letra != Letra.ToUpper())
+                    yield return new ValidationResult("La primer letra debe ser mayuscula",
+                        new string[] { nameof(Nombre) });
+            }
+        }
     }
 }
