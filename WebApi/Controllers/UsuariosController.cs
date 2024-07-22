de753a7965d52654c876212586359c913684ddc2
joaquinnoceti <joaquinnoceti@hotmail.com>
2024-07-22 11:56:07 -0300

    endpoint renovar token

--- WebApi/Controllers/UsuariosController.cs
+++ WebApi/Controllers/UsuariosController.cs
@@ -1,10 +1,13 @@
-﻿using Microsoft.AspNetCore.Identity;
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.IdentityModel.Tokens;
 using System;
 using System.Collections.Generic;
 using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
 using System.Security.Claims;
 using System.Text;
 using System.Threading.Tasks;
@@ -60,6 +63,20 @@ namespace WebApi.Controllers
             }
 
         }
+
+        [HttpGet]
+        [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
+        public ActionResult<RespuestaAutenticacion> RenovarToken()
+        {
+            var emailClaim = HttpContext.User.Claims.Where(claim => claim.Type == "email").FirstOrDefault();
+            var email = emailClaim.Value;
+            var credencialesUsuario = new CredencialesUsuario()
+            {
+                Email = email
+            };
+            return ConstruirToken(credencialesUsuario);
+        }
+
 
         private RespuestaAutenticacion ConstruirToken(CredencialesUsuario credencialesUsuario)
         {
